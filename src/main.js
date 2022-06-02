91e8081af700499bb37a0479b4685246c2850e92
Lachlan Rumery <99456821+LachlanRumery@users.noreply.github.com>
2022-06-03 07:42:55 +1000

    Semi-fixed bug by:     - innerTEXT -> innerHTML     - slice to max length

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 document.addEventListener("mouseup",event=>{
     var selected = document.getSelection();
-    var selectedNode = selected.baseNode.parentElement
+    var selectedNode;
+    try {
+        selectedNode = selected.baseNode.parentElement
+    } catch(error) {
+        return;
+    }
+    if(selected.toString()=='') {
+        return;
+    }
     var innerText = selectedNode.innerText
+    var innerHTML = selectedNode.innerHTML
 
-    var index = selectedNode.innerText.search(selected.toString())
-    selectedNode.innerHTML = `${innerText.slice(0,index)}<span id="text1">${selected.toString()}</span>${innerText.slice(index+selected.toString().length,innerText.length)}`
+    console.log(innerHTML)
+    console.log(selected.toString())
+
+    var index = selectedNode.innerHTML.search(selected.toString())
+    selectedNode.innerHTML = `${selectedNode.innerHTML.slice(0,index)}<span id="text1">${selected.toString()}</span>${selectedNode.innerHTML.slice(index+selected.toString().length, selectedNode.innerHTML.length)}`
 
     document.getElementById("text1").style.color = "red"
 })
